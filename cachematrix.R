b4a23017e4bfeaf5c5e984adf1968047f18f8b13
Abhijeet Gulati <agulati@qti.qualcomm.com>
2014-07-25 16:39:28 -0700

    Programming Assignment #2
    Programming Assignment #2 by Abhijeet Gulati

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+#####
+# Programming Assignment 2
+# Abhijeet Gulati 
+# version 1.0
+# July 2014
+# Function name: makeCacheMatrix
+# Function Arguments: accepts a square matrix
+# Details: Function to set/get/setinverse and getinverse 
+# of a cached square matrix
+# 1. Set the value of the square matrix
+# 2. get the value of the square matrix
+# 3. set the value of the square matrix
+# 4. get the value of the sqaure matrix
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(mat = matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
-
+     # Verify if the matrix is strict matrix and not any other object
+     if (!is.matrix(mat)){
+          print("Not a invertible matrix!")
+          return
+          }
+     else     
+     {
+          inverse <- NULL
+          set <- function(y) {
+               mat <<- y
+               inverse <<- NULL
+          }
+          get <- function() mat
+          set_inv <- function(mat_inverse) inverse <<- mat_inverse
+          get_inv <- function() inverse
+          list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
+     }
+     
 }
 
+#####
+# Programming Assignment 2
+# Abhijeet Gulati 
+# version 1.0
+# July 2014
+# Function name: cacheSolve
+# Function Arguments: strict square matrix
+# Details: Function checks for cached inverse matrix and if 
+# no cache exists, creates it
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(m, ...){
+     
+     mi<-m$get_inv()
+     if(!is.null(mi)) {
+          message("getting cached inverse matrix")
+          return(mi)
+     }
+     data<-m$get()
+     
+     mi<-solve(data) %*% data
+     
+     m$set_inv(mi)
+     mi
 }
